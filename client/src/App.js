45fbf50e2b0ae8f2cf8388f07d7c944386fe5d72
HabacucMX <habacuc.vera@gmail.com>
2022-07-18 23:54:58 -0500

    Past Trips screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import Hud from './components/Hud'
 //import Trip from './Screens/Trip'
-import ATrips from './Screens/AvaliableTrips'
+//import ATrips from './Screens/AvaliableTrips'
+import PTrips from './Screens/PastTrips'
 
 
 function App() {
   return (
     <div class="App">
       <Hud/>
-      <ATrips/>
+      <PTrips/>
     </div>
   );
 }
