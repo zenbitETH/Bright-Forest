71c2112db633860ef60b773bb06cb7c11e0ff3de
HabacucMX <habacuc.vera@gmail.com>
2022-07-24 20:57:34 -0500

    Router for external host

--- client/src/Components/Hud.js
+++ client/src/Components/Hud.js
@@ -1,46 +1,43 @@
 import Map from "./GoogleMap";
 function Hud() {
-  console.log(window.location);
-  return (
-    <div>
-      {/*<div class="topHud">
+    return (
+        <div>
+            {/*
+            <div class="topHud">
                 <div class="hudWallet">Connect</div>
-            </div> */}
-
-      <div class="topHud">
-        <div class="hudRider">Address/Rider</div>
-        {/*Data from the adress or Rider if there is Rider profiles*/}
-        <div class="hudEnergy">5⚡</div>
-        {/*Energy Units from Punk Cities*/}
-        <div class="hudPOAPs">2 🏅</div>
-        {/*Bright Forest POAPs*/}
-      </div>
-
-      <div class="bottomHud">
-        <div class="hudLeader">
-          {" "}
-          <button
-            onClick={() =>
-              window.location.assign("http://localhost:3000/leaderboard")
-            }
-          >
-            Leaderboard
-          </button>
+            </div> 
+            */}
+            <div class="topHud">
+                <div class="hudRider">0xdA7d...da9B6</div>{/*Data from the adress or Rider if there is Rider profiles*/}
+                <div class="hudEnergy">1,700⚡</div>{/*Energy Units from Punk Cities*/}
+            </div>
+            <div class="bottomHud">
+              <div class="hudLeader">
+                {" "}
+                <button
+                  onClick={() =>
+                    window.location.assign("/leaderboard")
+                  }
+                >
+                  Leaderboard
+                </button>
+              </div>
+              {/*All riders Leaderboard*/}
+              <div class="hudTrips">
+                  <button
+                  onClick={() =>
+                    window.location.assign("/pasttrips")
+                  }
+                >
+                Past/Current Trips{" "}
+                </button>
+                </div>
+              {/*Switch between current and past Trips*/}
+            </div>
+         
         </div>
-        {/*All riders Leaderboard*/}
-        <div class="hudTrips">
-            <button
-            onClick={() =>
-              window.location.assign("http://localhost:3000/pasttrips")
-            }
-          >
-          Past/Current Trips{" "}
-          </button>
-          </div>
-        {/*Switch between current and past Trips*/}
-      </div>
-    </div>
-  );
+    )
+
 }
 
 export default Hud;
