fe84a83d4a31496d24b5c2418a0385368cd6b5e3
Shyam Patel <shyampkira@gmail.com>
2022-09-12 00:28:01 +0530

    Major Refactor

---
+++ old/client/src/App.js
@@ -0,0 +1,36 @@
+import {BrowserRouter , Routes, Route} from"react-router-dom";
+import Hud from './Components/Hud';
+import Home from "./Screens/Home";
+import AvailableTrips from './Screens/AvaliableTrips'
+import Trip from './Screens/Trip'
+import PastTrips from './Screens/PastTrips'
+import Leaderboard from "./Screens/Leaderboard";
+import TripOne from './Components/TripStep1'
+import TripTwo from './Components/TripStep2'
+import TripThree from './Components/TripStep3'
+import TripFour from './Components/TripStep4'
+
+import Map from './Components/GoogleMap'
+
+function App() {
+  return (
+    <div class="App">
+      <Hud />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AvailableTrips />} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/trip2" element={<TripTwo />} />
+          <Route path="/trip3" element={<TripThree />} />
+          <Route path="/trip4" element={<TripFour />} />
+          <Route path="/pasttrips" element={<PastTrips />} />
+          <Route path="/maponly" element={<Map/>} />
+          <Route path="/leaderboard" element={<Leaderboard/>} />
+          <Route path="/trip" element={<Trip/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
